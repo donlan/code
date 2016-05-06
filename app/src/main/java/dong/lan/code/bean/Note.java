95471391d86f9e5a2ffb26d4f9e23ff0810d8d0e
dooze <760625325@qq.com>
2016-05-07 02:40:33 +0800

    Parcelable化 方便在事件中传送

--- app/src/main/java/dong/lan/code/bean/Note.java
+++ app/src/main/java/dong/lan/code/bean/Note.java
@@ -1,9 +1,12 @@
 package dong.lan.code.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Dooze on 2015/10/31.
  */
-public class Note {
+public class Note implements Parcelable {
 
     private String type;
     private String time;
@@ -41,4 +44,35 @@ public class Note {
     public void setNote(String note) {
         this.note = note;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.type);
+        dest.writeString(this.time);
+        dest.writeString(this.note);
+    }
+
+    protected Note(Parcel in) {
+        this.type = in.readString();
+        this.time = in.readString();
+        this.note = in.readString();
+    }
+
+    public static final Parcelable.Creator<Note> CREATOR = new Parcelable.Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel source) {
+            return new Note(source);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
 }
