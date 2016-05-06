38108e9b75ee289c622237813c4f408942b67d3f
dooze <760625325@qq.com>
2016-05-07 02:31:40 +0800

    拖动排序，删除

---
+++ app/src/main/java/dong/lan/code/utils/MyItemTouchHelper.java
@@ -0,0 +1,62 @@
+package dong.lan.code.utils;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import dong.lan.code.Interface.ItemTouchListener;
+
+
+public class MyItemTouchHelper extends ItemTouchHelper.Callback{
+
+
+
+
+    private ItemTouchListener touchListener;
+
+
+    public MyItemTouchHelper(ItemTouchListener touchListener) {
+        this.touchListener = touchListener;
+    }
+
+    public void setTouchListener(ItemTouchListener listener){
+        this.touchListener = listener;
+    }
+
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        int dragFlag = ItemTouchHelper.DOWN | ItemTouchHelper.UP;
+        int swipeFlag = ItemTouchHelper.END ;
+
+        return makeMovementFlags(dragFlag, swipeFlag);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+
+        touchListener.onItemMoved(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        touchListener.onItemSwiped(viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public float getSwipeThreshold(RecyclerView.ViewHolder viewHolder) {
+        return super.getSwipeThreshold(viewHolder);
+    }
+}
