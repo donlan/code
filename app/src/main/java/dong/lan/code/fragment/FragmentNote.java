1f2d50e54e386695a9141603af678a80ca075008
dooze <760625325@qq.com>
2016-05-07 02:38:09 +0800

    加入记事闹钟提醒功能

--- app/src/main/java/dong/lan/code/fragment/FragmentNote.java
+++ app/src/main/java/dong/lan/code/fragment/FragmentNote.java
@@ -1,6 +1,8 @@
 package dong.lan.code.fragment;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,8 +13,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,30 +26,31 @@ import dong.lan.code.Interface.NoteItemClickListener;
 import dong.lan.code.Interface.OnNoteChangeListener;
 import dong.lan.code.MainActivity;
 import dong.lan.code.R;
+import dong.lan.code.activity.NoteAlarmActivity;
 import dong.lan.code.adapter.NoteAdapter;
 import dong.lan.code.bean.Note;
 import dong.lan.code.db.DBManeger;
+import dong.lan.code.utils.AlarmUtils;
 import dong.lan.code.utils.DividerItemDecoration;
 import dong.lan.code.utils.TimeUtil;
+import dong.lan.code.utils.ToastUtil;
 
 /**
  * 项目：code
  * 作者：梁桂栋
  * 日期： 2015/10/31  12:43.
  */
-public class FragmentNote extends BaseFragment implements View.OnClickListener, NoteItemClickListener,OnNoteChangeListener {
+public class FragmentNote extends BaseFragment implements View.OnClickListener, NoteItemClickListener, OnNoteChangeListener {
 
     public static final String NO_NOTE = "添加一个记事";
     private RecyclerView recyclerView;
     private NoteAdapter adapter;
     private NoteAdapter rAdapter;
-    private TextView addNote;
     private EditText searchText;
 
-    private List<Note> notes = new ArrayList<>();
     private boolean resetAdapter;
     private boolean isSearch;
-    private boolean isCopy=false;
+    private boolean isCopy = false;
 
 
     @Override
@@ -62,15 +67,15 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
     private void initView() {
         MainActivity.setOnNoteChangeListenner(this);
         recyclerView = (RecyclerView) getView().findViewById(R.id.note_recyclerView);
-        addNote = (TextView) getView().findViewById(R.id.note_add);
+        TextView addNote = (TextView) getView().findViewById(R.id.note_add);
         searchText = (EditText) getView().findViewById(R.id.note_search_et);
         addNote.setOnClickListener(this);
-        adapter = new NoteAdapter(getActivity(),null);
+        adapter = new NoteAdapter(getActivity(), null);
         rAdapter = new NoteAdapter(getActivity(), null);
         rAdapter.setNoteItemClickListener(new NoteItemClickListener() {
             @Override
             public void onNoteClick(Note note, int pos) {
-                isCopy=true;
+                isCopy = true;
                 detailDialog(note, pos);
             }
 
@@ -102,7 +107,7 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
                         resetAdapter = false;
                     }
                 } else if (isSearch) {
-                   // rAdapter.delAddAll(notes);
+                    // rAdapter.delAddAll(notes);
 //                    recyclerView.setAdapter(rAdapter);
                     isSearch = false;
                     resetAdapter = true;
@@ -119,7 +124,7 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
 
             }
         });
-        notes = DBManeger.getInstance().getAllNote();
+        List<Note> notes = DBManeger.getInstance().getAllNote();
         if (notes == null || notes.isEmpty()) {
             Show("没有记事笔记 !!!");
             adapter = new NoteAdapter(getActivity(), null);
@@ -128,53 +133,31 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
         } else {
             adapter = new NoteAdapter(getActivity(), notes);
             recyclerView.setAdapter(adapter);
-            //设置item的点击监听
             adapter.setNoteItemClickListener(this);
         }
         //设置布局管理
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1, GridLayoutManager.VERTICAL, false);
-        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(gridLayoutManager);
-        //设置分割线divider
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
-        //设置item的添加删除的动画
         recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     AlertDialog dialog;
 
     private void detailDialog(final Note n, final int pos) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.detail_note_dialog, null);
-        final EditText text = (EditText) view.findViewById(R.id.dNote_des);
-        TextView type = (TextView) view.findViewById(R.id.dNote_type);
-        TextView time = (TextView) view.findViewById(R.id.dNote_time);
-        Button done = (Button) view.findViewById(R.id.reedit_button);
-        text.setText(n.getNote());
-        text.setSelection(n.getNote().length());
-        time.setText(n.getTime());
-        type.setText(n.getType());
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!text.getText().toString().equals(n.getNote())) {
-                    Note note = new Note("记事", TimeUtil.dateToString(new Date(), TimeUtil.FORMAT_DATA_TIME_SECOND_1), text.getText().toString());
-                    if(isCopy)
-                    rAdapter.updateNote(n.getTime(), note, pos);
-                    else
-                        adapter.updateNote(n.getTime(), note, pos);
-                }
-                dialog.dismiss();
-            }
-        });
-        builder.setView(view);
-        dialog = builder.show();
+        MainActivity.LOCK_STATUS = false;
+        Intent intent = new Intent(getActivity(), NoteAlarmActivity.class);
+        intent.putExtra("NOTE",n);
+        intent.putExtra("POS",pos);
+        startActivityForResult(intent,0);
+
     }
 
     private void addDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.add_note_dialog, null);
         final EditText text = (EditText) view.findViewById(R.id.add_note_des);
+        final EditText type = (EditText) view.findViewById(R.id.add_note_type);
         TextView done = (TextView) view.findViewById(R.id.add_note_done);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -183,11 +166,21 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
                     Show("没有笔记!!!");
                     return;
                 }
+                String typeText = (type.getText().toString());
+                if (typeText.length() > 10) {
+                    ToastUtil.Show(getActivity(), "只能是10个汉字以内的自定义类型");
+                    return;
+                }
                 Note n = new Note();
-                n.setType("记事");
+                if (typeText.equals(""))
+                    n.setType("记事");
+                else {
+                    n.setType(typeText);
+                }
                 n.setNote(text.getText().toString());
                 n.setTime(TimeUtil.dateToString(new Date(), TimeUtil.FORMAT_DATA_TIME_SECOND_1));
                 adapter.addNote(n);
+                recyclerView.scrollToPosition(0);
                 dialog.dismiss();
             }
         });
@@ -207,24 +200,78 @@ public class FragmentNote extends BaseFragment implements View.OnClickListener, 
 
     @Override
     public void onNoteClick(Note note, int pos) {
-        isCopy=false;
+        isCopy = false;
         if (note == null) {
             addDialog();
-        } else
+        } else{
+            System.out.println(note.getNote());
             detailDialog(note, pos);
+        }
     }
 
     @Override
-    public void onNoteLongClick(Note note, int pos) {
-        adapter.deleteNote(pos);
+    public void onNoteLongClick(Note note, final int pos) {
+        new AlertDialog.Builder(getActivity()).setTitle("选择你需要的操作")
+                .setPositiveButton("闹钟提醒", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        pickerTime(adapter.getNoteAt(pos));
+                    }
+                })
+                .setNegativeButton("删除记事", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        adapter.deleteNote(pos);
+                    }
+                }).show();
     }
+    public void pickerTime(final Note info) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_picker_time, null);
+        final CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        final TimePicker timePicker = (TimePicker) view.findViewById(R.id.timePicker);
+        Button ok = (Button) view.findViewById(R.id.pick_ok);
+        final Date date = new Date(System.currentTimeMillis());
+        calendarView.setDate(date.getTime());
+        timePicker.setIs24HourView(true);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                date.setHours(timePicker.getCurrentHour());
+                date.setMinutes(timePicker.getCurrentMinute());
+                long triggerTime = date.getTime() - System.currentTimeMillis();
+                if (triggerTime<=0) {
+                    Show("提醒时间不能设置在当前时间之前");
+                    return;
+                }
+                AlarmUtils.addAlarm(getActivity(), triggerTime, info);
+                dialog.dismiss();
+            }
+        });
 
+        builder.setView(view);
+        dialog = builder.show();
+    }
 
     @Override
     public void onNoteChange(int Tag) {
-        if(Tag ==1)
-        {
+        if (Tag == 1) {
             searchText.setText("");
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==0 && resultCode==1){
+
+            Note note = data.getParcelableExtra("NOTE");
+            int pos = data.getIntExtra("POS",-1);
+            if(pos!=-1 && note!=null){
+                if (isCopy)
+                    rAdapter.updateNote(note.getTime(), note, pos);
+                else
+                    adapter.updateNote(note.getTime(), note, pos);
+            }
         }
     }
 }
